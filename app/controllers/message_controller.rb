d939bf8c45dadefef9a4c22269220ed5d8ce96c3
michaeldtodd <mtodd@cloverhound.com>
2015-10-22 11:29:11 -0400

    real hello world

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,5 @@
+class MessageController < ApplicationController
+	def index
+		Tropo::Generator.say 'Hello World!'
+	end
+end
---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+require 'tropo-webapi-ruby'
+# Initialize the Rails application.
+Rails.application.initialize!
