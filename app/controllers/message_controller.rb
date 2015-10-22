e443299bfef8a034b325e10f28c3275096db67fc
michaeldtodd <mtodd@cloverhound.com>
2015-10-22 12:02:51 -0400

    changing to exact example

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,5 +1,27 @@
 class MessageController < ApplicationController
-	def index
-		render :json => Tropo::Generator.say 'Hello World!'
-	end
+	  def index
+    #initial_text captures the very first sms or IM sent to tropo
+    initial_text = params["session"]["initialText"]
+    from = params["session"]["from"]
+    network = from["network"]
+    from_id = from["id"] # this field contains IM login or phone number in case of incoming SMS
+    if network == "SMS" || network == "JABBER"
+      render :json => parse(initial_text)
+    else
+      render :json => Tropo::Generator.say("Unsupported operation")
+    end
+  end
+
+  private
+
+  def parse(input)
+    input.strip!
+    # do whatever parsing you need.  in this example, if user types "n what a new day", tropo will
+    # respond him with "you said: what a new day"
+    if m = input.match(/^(n|N)\s+/)
+      Tropo::Generator.say "you said: " + m.post_match
+    else
+      Tropo::Generator.say "Unsupported operation."
+    end
+  end
 end
